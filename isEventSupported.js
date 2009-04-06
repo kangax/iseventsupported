1a6cd42b84ec748b146821893f557c3bf6ad5284
Juriy Zaytsev <juriy@kangax.local>
2009-04-06 13:30:08 -0400

    Initial commit

---
+++ isEventSupported.js
@@ -0,0 +1,26 @@
+var isEventSupported = (function(){
+  var TAGNAMES = {
+    'select':'input','change':'input',
+    'submit':'form','reset':'form',
+    'error':'img','load':'img','abort':'img'
+  }, cache = { };
+  function isEventSupported(eventName, element) {
+    var canCache = arguments.length == 1;
+    // only return cached result when no element is given
+    if (canCache && cache[eventName]) {
+      return cache[eventName];
+    }
+    var el = element || document.createElement(TAGNAMES[eventName] || 'div');
+    eventName = 'on' + eventName;
+    // When using `setAttribute`, IE skips "unload", WebKit skips "unload" and "resize"
+    // `in` "catches" those
+    var isSupported = (eventName in el);
+    if (!isSupported) {
+      el.setAttribute(eventName, 'return;');
+      isSupported = typeof el[eventName] == 'function';
+    }
+    el = null;
+    return canCache ? (cache[eventName] = isSupported) : isSupported;
+  }
+  return isEventSupported;
+})();
---
+++ test.js
@@ -0,0 +1,97 @@
+function w(name) {
+  document.write(name + ': ' + (
+    isEventSupported(name) 
+      ? '<span style="background-color:green;color:white;">true</span>' 
+      : '<span style="background-color:red;color:white;">false</span>'
+    ) + '<br>');
+}
+
+document.write('<pre>' + isEventSupported + '</pre><br><br>');
+
+document.write('<h2>Mouse events:</h2>');
+
+w('click');
+w('dblclick');
+w('mousedown');
+w('mouseup');
+w('mouseover');
+w('mousemove');
+w('mouseout');
+
+document.write('<h2>Key events:</h2>');
+
+w('keypress');
+w('keydown');
+w('keyup');
+
+document.write('<h2>HTML events</h2>');
+
+w('load');
+w('unload');
+w('abort');
+w('error');
+
+document.write('<h2>View events</h2>');
+
+w('resize');
+w('scroll');
+
+document.write('<h2>Form events:</h2>');
+
+w('submit');
+w('reset');
+
+document.write('<h2>Form controls\' events:</h2>');
+
+w('select');
+w('change');
+
+document.write('<h2>Activation events:</h2>');
+
+w('focus');
+w('blur');
+
+document.write('<h2>MSHTML DOM events:</h2>');
+
+w('cut');
+w('copy');
+w('paste');
+w('beforecut');
+w('beforecopy');
+w('beforepaste');
+w('afterupdate');
+w('beforeupdate');
+w('cellchange');
+w('dataavailable');
+w('datasetchanged');
+w('datasetcomplete');
+w('errorupdate');
+w('rowenter');
+w('rowexit');
+w('rowsdelete');
+w('drag');
+w('dragstart');
+w('dragenter');
+w('dragover');
+w('dragleave');
+w('dragend');
+w('drop');
+w('selectstart');
+w('mouseenter');
+w('mouseleave');
+w('activate');
+w('beforeactivate');
+w('deactivate');
+w('beforedeactivate');
+w('focusin');
+w('focusout');
+w('stop');
+
+document.write('<h2>Unexistent (most likely) events:</h2>');
+
+w('click2');
+w('foobarbaz');
+
+document.write('<h2>Unexistent in Opera <=10a</h2>');
+
+w('contextmenu');
